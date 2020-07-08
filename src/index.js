d94a87d61a496e511b0a98753115be750aee8f75
carlossm50 <37375440+carlossm50@users.noreply.github.com>
2020-07-08 18:36:19 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
-const http = require('./app');
+const path = require('path');
+const express = require("express");
+const app = express();
 
-http.listen(3000,()=>{
-    console.log("server on por 3000");
+//Settings
+app.set("port", process.env.PORT || 3000);
+
+//Static files
+app.use(express.static(path.join(__dirname, "public")));
+
+//Start server
+const server = app.listen(app.get("port"), () => {
+    console.log("Sever on por", app.get("port"))
+});
+
+//Web Socket
+const SocketiIO = require("socket.io");
+const io = SocketiIO(server);
+
+io.on('connection', (socket) => {
+    socket.on('stream', (imege) => {
+        socket.broadcast.emit('stream',imege);
+    })
 })
