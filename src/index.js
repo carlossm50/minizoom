11de1df9a03fbaaf7502fce78eca62ca5b75625c
CARLOS APOLINAR SEVERINO MEJIA <D19A0006@educacion.edu.do>
2020-07-20 15:41:36 -0400

    video call peer to perr

--- src/index.js
+++ src/index.js
@@ -16,9 +16,28 @@ const server = app.listen(app.get("port"), () => {
 //Web Socket
 const SocketiIO = require("socket.io");
 const io = SocketiIO(server);
-
+let countpeer = 0;
 io.on('connection', (socket) => {
-    socket.on('stream', (imege) => {
-        socket.broadcast.emit('stream',imege);
+    socket.on('newpeer', () => {
+        if (countpeer < 2) {
+            countpeer ++;
+            if (countpeer === 2) {
+                io.emit('createpeer');
+            }
+        }
+    })
+
+    socket.on('P1sendToBack', function (data) {
+        socket.broadcast.emit('P1SendToFront', data);
+    })
+
+    socket.on('P2sendToBack', function (data) {
+        socket.broadcast.emit('P2SendToFront', data);
+    })
+
+    socket.on('disconnect', function Disconnect() {
+        if (countpeer > 0) {
+            countpeer--;
+        }
     })
 })
